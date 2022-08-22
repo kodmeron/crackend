36c4f5f978069239120a07af940d339128abe7a3
kodmeron <cameron.b.lloyd@gmail.com>
2022-08-22 13:52:53 +0200

    nuså

--- server.js
+++ server.js
@@ -1,10 +1,19 @@
 const express = require("express")
+
 const app = express()
 
 const PORT = process.env.PORT || 3000
+
 
 app.get('/', (req, res) => {
   res.status(200).send('Shorty all good ahah')
 })
 
-app.listen(port, console.log('baby we up'))
+app.post("/", (req, res) => {
+  res.send("Baby d post request hours")
+});
+
+
+app.listen(PORT, () => {
+  console.log(`Servern kutar fan sönder på port ${PORT}`);
+});
